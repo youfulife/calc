1ec55eff803ae62ee02750c3005d6e733dbd7ee5
chenyoufu <youfu.ok@163.com>
2016-07-04 22:44:13 +0800

    rename calc0 to lpi-->lexer,parser,interpreter, update rpn using visitor pattern

--- rpn.py
+++ rpn.py
@@ -1,92 +1,34 @@
 import unittest
-from calc0 import Token, Lexer
-
-INTEGER = "INTEGER"
-PLUS, MINUS, MUL, DIV, EOF = "PLUS", "MINUS", "MUL", "DIV", "EOF"
-LPAREN, RPAREN = "LPAREN", "RPAREN"
+from lpi import Lexer, Parser, NodeVisitor
 
 
-class Node(object):
-    def __init__(self, token, left, right):
-        self.token = token
-        self.left = left
-        self.right = right
+class Infix2PostfixTranslator(NodeVisitor):
+    def __init__(self, tree):
+        self.tree = tree
+
+    def visit_BinOp(self, node):
+        left_val = self.visit(node.left)
+        right_val = self.visit(node.right)
+        return '{left} {right} {op}'.format(
+            left=left_val,
+            right=right_val,
+            op=node.op.value
+        )
+
+    def visit_Num(self, node):
+        return node.value
+
+    def translate(self):
+        return self.visit(self.tree)
 
 
-class Parser(object):
-    """
-    expr: term((PLUS|MINUS)term)*
-    term: factor((MUL|DIV)factor)*
-    factor: INTEGER|LP expr RP
-    """
-    def __init__(self, lexer):
-        self.lexer = lexer
-        self.token = self.lexer.get_next_token()
-
-    def error(self):
-        raise Exception("parsing error")
-
-    def eat(self, token_type):
-        if self.token.type == token_type:
-            self.token = self.lexer.get_next_token()
-        else:
-            print token_type
-            print self.token
-            self.error()
-
-    def expr(self):
-        node = self.term()
-        while self.token.type in (PLUS, MINUS):
-            op = self.token
-            if op.type == PLUS:
-                self.eat(PLUS)
-            elif op.type == MINUS:
-                self.eat(MINUS)
-            node = Node(op, node, self.term())
-        return node
-
-    def term(self):
-        node = self.factor()
-        while self.token.type in (MUL, DIV):
-            op = self.token
-            if op.type == MUL:
-                self.eat(MUL)
-            elif op.type == DIV:
-                self.eat(DIV)
-            node = Node(op, node, self.factor())
-        return node
-
-    def factor(self):
-        if self.token.type == INTEGER:
-            node = Node(self.token, None, None)
-            self.eat(INTEGER)
-            return node
-        elif self.token.type == LPAREN:
-            self.eat(LPAREN)
-            node = self.expr()
-            self.eat(RPAREN)
-            return node
-
-    def parse(self):
-        return self.expr()
-
-def visit(tree):
-    if tree.left:
-        visit(tree.left)
-    if tree.right:
-        visit(tree.right)
-    stack.append(tree.token.value)
-
-stack = []
 def infix2postfix(s):
-    global stack
     lexer = Lexer(s)
     parser = Parser(lexer)
     tree = parser.parse()
-    visit(tree)
-    rpn = ' '.join(map(str, stack))
-    stack = []
-    return rpn
+    translator = Infix2PostfixTranslator(tree)
+    translation = translator.translate()
+    return translation
 
 class Infix2PostfixTestCase(unittest.TestCase):
 
